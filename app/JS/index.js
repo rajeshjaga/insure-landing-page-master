efc549d80da8e5afb102d738fc21f3f7cf3672e2
Lucifer___13 <rajaga223@gmail.com>
2021-04-12 09:40:25 +0530

    commit: Shifting os

--- app/JS/index.js
+++ app/JS/index.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuOpen = document.getElementById("menu-open");
-    const menuClose = document.getElementById("menu-close");
-    const navLists = document.querySelector('.nav-container');
-    menuOpen.addEventListener('click', () => {
-        navLists.classList.add('open')
-    })
-    menuClose.addEventListener('click', () => {
-        navLists.classList.remove('open')
-    })
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const menuOpen = document.getElementById("menu-open");
+  const menuClose = document.getElementById("menu-close");
+  const navLists = document.querySelector(".nav-container");
+  menuOpen.addEventListener("click", () => {
+    navLists.classList.add("open");
+  });
+  menuClose.addEventListener("click", () => {
+    navLists.classList.remove("open");
+  });
+});
